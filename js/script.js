071082e27df8f750695038227b7baacd3b4ea9d2
Eldho Shaji <me@eldhoshaji.com>
2022-01-09 20:41:53 +0530

    landing page

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.second-button').addEventListener('click', function () {
+    document.querySelector('.animated-icon').classList.toggle('open');
+//     document.querySelector('#navbarToggleExternalContent10').classList.toggle('collapse')
+});
