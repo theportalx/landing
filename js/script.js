db0370e4bc8cbef1ed26499fc556f55fda587927
Eldho Shaji <me@eldhoshaji.com>
2022-01-11 01:13:14 +0530

    navigate to app functionality added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
+// baseUrl = "http://localhost:4200/"
+baseUrl = "https://app.portalx667cc.com/"
+
+
 document.querySelector('.second-button').addEventListener('click', function () {
     document.querySelector('.animated-icon').classList.toggle('open');
 //     document.querySelector('#navbarToggleExternalContent10').classList.toggle('collapse')
 });
+
+document.querySelector('.claim-button').addEventListener('click', function () {
+    var username = document.getElementById("username").value;
+    if(username.length > 3){
+        window.open(baseUrl + 'auth/signup?username=' + username, '_blank');
+    }
+});
+
+document.querySelector('.early-access-button').addEventListener('click', function () {
+    window.open(baseUrl + 'auth/signup', '_blank');
+});
+
